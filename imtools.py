a73eb2930a75b9f4d3f36fee6160312acd3f5eb3
Pramit Gopal Yeole <pramit998@gmail.com>
2023-05-19 22:39:54 -0400

    Delete imtools.py
    Demo upload

--- imtools.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_imlist(path):
-    """Returns a list of filelnames 
-       for jpg images in a directory"""
-    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
